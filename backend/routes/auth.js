2c5b3dcd55de13a720d771e87ccd05705aa50878
notslok <53101134+notslok@users.noreply.github.com>
2023-04-19 22:20:03 +0530

    validation + bcryptjs + salt&pepper implemented

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+// Create a User using: POST "/api/auth/createuser" - login not needed
+router.post('/createuser', [ check('name').isLength({min : 3}), check('email').isEmail(), check('password').isLength({min : 5})], async (req,res)=>{
+    
+    // check for errors
+    const error = validationResult(req);
+    if (!error.isEmpty()) { 
+      return res.status(400).json({errors: error.array()});
+    } 
 
-// Create a User using: POST "/api" - auth not needed
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    
-    const user = User(req.body);
-    user.save();
-    
-    res.send(req.body);
+    // check whether user email lready exists
+    try{
+    let user = await User.findOne({email:req.body.email}); 
+    if(user){
+        return res.status(400).json({error: "Sorry, a user with this email already exists"})
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    securePassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create user
+    user = await User.create({
+        name: req.body.name,
+        password: securePassword,
+        email: req.body.email
+    })
+    res.json(user)
+
+    } catch(error){
+     
+        console.error(error.message);
+        res.status(500).send("Uh-oh! Some error occured");
+    }
 })
 
 module.exports = router;
